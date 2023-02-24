4a41ebae16865c2b5a618036c7eac93415186141
kibdev <31766430+kibmuikia@users.noreply.github.com>
2023-02-24 12:17:03 +0300

    chore: configured splash-screen lottie animation.
    Signed-off-by: kibdev <31766430+kibmuikia@users.noreply.github.com>

--- app/src/main/java/kib/project/fast/ui/splash/SplashScreen.kt
+++ app/src/main/java/kib/project/fast/ui/splash/SplashScreen.kt
@@ -1,23 +1,23 @@
 package kib.project.fast.ui.splash
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.LottieConstants
+import com.airbnb.lottie.compose.animateLottieCompositionAsState
+import com.airbnb.lottie.compose.rememberLottieComposition
 import kib.project.fast.R
 import kib.project.fast.navigation.Graph
 import kotlinx.coroutines.delay
@@ -29,7 +29,7 @@ fun SplashScreen(navHostController: NavHostController) {
 
     LaunchedEffect(key1 = true) {
         val route = if (viewModel.isUserFirstVisit.value) Graph.ON_BOARDING else Graph.MAIN
-        delay(1500L)
+        delay(2000L)
         navHostController.navigate(route) {
             popUpTo(Graph.SPLASH) {
                 inclusive = true
@@ -41,6 +41,14 @@ fun SplashScreen(navHostController: NavHostController) {
 
 @Composable
 fun SplashScreenContent() {
+    val composition by rememberLottieComposition(LottieCompositionSpec.RawRes(R.raw.lottie_meditating_rabbit))
+    val progress by animateLottieCompositionAsState(
+        composition = composition,
+        iterations = LottieConstants.IterateForever,
+        reverseOnRepeat = true,
+        speed = 1.5F,
+    )
+
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -48,15 +56,15 @@ fun SplashScreenContent() {
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
-        Text(
-            text = "Fast",
-            fontSize = 60.sp,
-            color = MaterialTheme.colorScheme.onTertiary,
-        )
-        Spacer(modifier = Modifier.height(20.dp))
-        Image(
-            painter = painterResource(id = R.drawable.ic_splat),
-            contentDescription = "Splash Logo"
+        // Image(
+        //  painter = painterResource(id = R.drawable.ic_splat),
+        //  contentDescription = "Splash Logo"
+        // )
+        LottieAnimation(
+            composition = composition,
+            progress = { progress },
+            modifier = Modifier.fillMaxSize(0.8F),
+            contentScale = ContentScale.Inside
         )
     }
 }
