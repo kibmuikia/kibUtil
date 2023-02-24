d7be058f2b57c558ebfc4961130ede8576937656
kibdev <31766430+kibmuikia@users.noreply.github.com>
2023-02-25 00:23:09 +0300

    chore: init config update to project build.gradle.kts & settings.gradle.kts
    Signed-off-by: kibdev <31766430+kibmuikia@users.noreply.github.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,28 @@
-buildscript {
-    repositories {
-        google()
-        mavenCentral()
-        maven { url = uri("https://jitpack.io") }
-    }
-}// Top-level build file where you can add configuration options common to all sub-projects/modules.
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
 
 plugins {
     id("com.android.application") version "7.4.1" apply false
 //    alias(libs.plugins.android.application)
     id("com.android.library") version "7.4.1" apply false
     id("org.jetbrains.kotlin.android") version "1.8.0" apply false
+    //id("com.diffplug.spotless.spotless-plugin-gradle") version "3.27.0"
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+        maven(url = "https://jitpack.io")
+    }
+}
+
+buildscript {
+    dependencies {
+        //
+    }
+    repositories {
+        mavenCentral()
+    }
 }
 
 tasks.register("clean", Delete::class){
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,14 +6,14 @@ pluginManagement {
         maven { url = uri("https://jitpack.io") }
     }
 }
-dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    repositories {
-        google()
-        mavenCentral()
-        maven { url = uri("https://jitpack.io") }
-    }
-}
+//dependencyResolutionManagement {
+//    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+//    repositories {
+//        google()
+//        mavenCentral()
+//        maven { url = uri("https://jitpack.io") }
+//    }
+//}
 rootProject.name = "Fast"
 include(":app")
 include(":domain")
