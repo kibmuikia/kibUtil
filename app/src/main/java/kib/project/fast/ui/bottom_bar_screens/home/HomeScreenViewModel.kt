2bcf7fe07d63e6ba57c040036a6cb66439cfac63
kibdev <31766430+kibmuikia@users.noreply.github.com>
2023-03-08 01:16:59 +0300

    chore: initial setup of sample api call. to-fix: Instance creation error : could not create instance for '[Singleton:'kib.project.data.database.AppDatabase']': java.lang.RuntimeException: Cannot find implementation for kib.project.data.database.AppDatabase. AppDatabase_Impl does not exist.
    Signed-off-by: kibdev <31766430+kibmuikia@users.noreply.github.com>

--- app/src/main/java/kib/project/fast/ui/bottom_bar_screens/home/HomeScreenViewModel.kt
+++ app/src/main/java/kib/project/fast/ui/bottom_bar_screens/home/HomeScreenViewModel.kt
@@ -1,9 +1,28 @@
 package kib.project.fast.ui.bottom_bar_screens.home
 
 import androidx.lifecycle.ViewModel
+import kib.project.data.api.models.requests.SampleLoginUserRequest
+import kib.project.data.api.models.responses.SampleUserResponse
+import kib.project.data.database.repositories.UserRepository
 import timber.log.Timber
 
-class HomeScreenViewModel : ViewModel() {
+class HomeScreenViewModel(
+    private val userRepository: UserRepository,
+) : ViewModel() {
+
+    suspend fun sampleLoginUser() {
+        try {
+            val sampleLoginUserRequest = SampleLoginUserRequest(
+                username = "Person Doe",
+                password = "1234"
+            )
+            val sampleUserResponse: SampleUserResponse =
+                userRepository.sampleLoginUser(sampleLoginUserRequest = sampleLoginUserRequest)
+            Timber.i(":: sampleUserResponse[ $sampleUserResponse ]")
+        } catch (exception: Exception) {
+            Timber.e(exception)
+        }
+    }
 
     init {
         Timber.i(":: vm-init")
--- data/src/main/java/kib/project/data/database/repositories/UserRepository.kt
+++ data/src/main/java/kib/project/data/database/repositories/UserRepository.kt
@@ -1,6 +1,9 @@
 package kib.project.data.database.repositories
 
 import User
+import kib.project.data.api.interfaces.SampleApi
+import kib.project.data.api.models.requests.SampleLoginUserRequest
+import kib.project.data.api.models.responses.SampleUserResponse
 import kib.project.data.database.dao.UserDao
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
@@ -20,10 +23,13 @@ interface UserRepository {
 
     fun getUserById(userId: String): Flow<User?>
 
+    suspend fun sampleLoginUser(sampleLoginUserRequest: SampleLoginUserRequest): SampleUserResponse
+
 }
 
 class UserRepositoryImpl(
     private val userDao: UserDao,
+    private val sampleApi: SampleApi,
     private val ioDispatcher: CoroutineDispatcher = Dispatchers.IO
 ): UserRepository { // KoinComponent
 
@@ -48,5 +54,7 @@ class UserRepositoryImpl(
     )
 
     override fun getUserById(userId: String): Flow<User?> = userDao.fetchUserByUserId(userId = userId)
+    override suspend fun sampleLoginUser(sampleLoginUserRequest: SampleLoginUserRequest): SampleUserResponse =
+        sampleApi.sampleLoginUser(sampleLoginUserRequest = sampleLoginUserRequest)
 
 }
