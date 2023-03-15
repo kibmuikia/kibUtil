96c341b6b9ec8d44910e9603adb8e201ff0f8038
kibdev <31766430+kibmuikia@users.noreply.github.com>
2023-03-15 07:01:22 +0300

    chore: lint: remove wildcard.
    Signed-off-by: kibdev <31766430+kibmuikia@users.noreply.github.com>

--- app/src/test/java/kib/project/fast/ExampleUnitTest.kt
+++ app/src/test/java/kib/project/fast/ExampleUnitTest.kt
@@ -1,8 +1,7 @@
 package kib.project.fast
 
+import org.junit.Assert.assertEquals
 import org.junit.Test
-
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
