5a995f2f9f5bdcb04766122cae4438f2241ae50e
kibdev <31766430+kibmuikia@users.noreply.github.com>
2023-03-15 05:33:55 +0300

    chore: added AboutScreen.
    Signed-off-by: kibdev <31766430+kibmuikia@users.noreply.github.com>

---
+++ app/src/main/java/kib/project/fast/ui/bottom_bar_screens/about/AboutScreen.kt
@@ -0,0 +1,61 @@
+package kib.project.fast.ui.bottom_bar_screens.about
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavHostController
+import kib.project.fast.R
+import org.koin.androidx.compose.getViewModel
+
+@Composable
+fun AboutScreen(navHostController: NavHostController) {
+    val viewModel: AboutScreenViewModel = getViewModel()
+    val coroutineScope = rememberCoroutineScope()
+    val context = LocalContext.current
+
+    AboutScreenContent()
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun AboutScreenContent() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = MaterialTheme.colorScheme.primaryContainer)
+    ) {
+        TopAppBar(
+            title = {
+                Text(
+                    text = stringResource(id = R.string.title_about),
+                    color = MaterialTheme.colorScheme.onPrimary,
+                    fontWeight = FontWeight.Medium,
+                    fontSize = 24.sp,
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis,
+                )
+            },
+            colors = TopAppBarDefaults.topAppBarColors(containerColor = MaterialTheme.colorScheme.primary),
+        )
+    }
+}
+
+@Composable
+@Preview
+private fun AboutScreenContentPreview() {
+    AboutScreenContent()
+}
