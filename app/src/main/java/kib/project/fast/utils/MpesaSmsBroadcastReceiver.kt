c7658486757cc76acac305d9d64a0e24d9b0512f
kibdev <31766430+kibmuikia@users.noreply.github.com>
2023-03-27 07:55:15 +0300

    chore: init MpesaSmsBroadcastReceiver.kt
    Signed-off-by: kibdev <31766430+kibmuikia@users.noreply.github.com>

---
+++ app/src/main/java/kib/project/fast/utils/MpesaSmsBroadcastReceiver.kt
@@ -0,0 +1,51 @@
+package kib.project.fast.utils
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.provider.Telephony
+import timber.log.Timber
+
+class MpesaSmsBroadcastReceiver: BroadcastReceiver() {
+    private var mpesaSmsReceiveListener: MpesaSmsReceiveListener? = null
+
+    interface MpesaSmsReceiveListener {
+        fun onMpesaSmsReceived(mpesaSms: String)
+    }
+
+    fun init(mpesaSmsReceiveListener: MpesaSmsReceiveListener) {
+        this.mpesaSmsReceiveListener = mpesaSmsReceiveListener
+    }
+
+    override fun onReceive(context: Context?, intent: Intent?) {
+        when {
+            context == null -> {
+                Timber.i(":: invalid context")
+                return
+            }
+            intent == null -> {
+                Timber.i(":: invalid intent")
+                return
+            }
+            intent.action == null -> {
+                Timber.i(":: invalid intent.action")
+                return
+            }
+            else -> processOnReceive(context, intent)
+        }
+    }
+
+    private fun processOnReceive(context: Context, intent: Intent) {
+        intent.action?.let { action ->
+            if (action == Telephony.Sms.Intents.SMS_RECEIVED_ACTION) {
+                val smsMessages = Telephony.Sms.Intents.getMessagesFromIntent(intent)
+                Timber.i(":: received ${smsMessages.size} smsMessages")
+                for (sms in smsMessages) {
+                    val messageBody = sms.messageBody
+                    Timber.i(":: sms: $sms -- \n$messageBody")
+                    // process messageBody
+                }
+            }
+        }
+    }
+}
