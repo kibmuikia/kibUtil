c268b7b0763e21046a1b336e7747fa05a7ea8f39
Allan Muikia <31766430+kibmuikia@users.noreply.github.com>
2023-03-30 02:27:48 +0300

    chore: port over to main branch files; ContextExtensions.kt and SystemBroadcastReceiver.kt (#17)
    Signed-off-by: kibdev <31766430+kibmuikia@users.noreply.github.com>

---
+++ app/src/main/java/kib/project/fast/ui/component/SystemBroadcastReceiver.kt
@@ -0,0 +1,42 @@
+package kib.project.fast.ui.component
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberUpdatedState
+
+/*
+* Generic Composable to register/unregister broadcast receivers & handle onReceive()
+* */
+@Composable
+fun SystemBroadcastReceiver(
+    context: Context,
+    systemAction: String, // Sample: Telephony.Sms.Intents.SMS_RECEIVED_ACTION
+    onSystemEvent: (intent: Intent) -> Unit
+) {
+    // Safely use the latest onSystemEvent lambda passed to the function
+    val currentOnSystemEvent by rememberUpdatedState(onSystemEvent)
+
+    // If either context or systemAction changes, unregister and register again
+    DisposableEffect(context, systemAction) {
+        val intentFilter = IntentFilter(systemAction)
+        val broadcast = object : BroadcastReceiver() {
+            override fun onReceive(context: Context?, intent: Intent?) {
+                intent?.let {
+                    currentOnSystemEvent(it)
+                }
+            }
+        }
+
+        context.registerReceiver(broadcast, intentFilter)
+
+        // When the effect leaves the Composition, remove the callback
+        onDispose {
+            context.unregisterReceiver(broadcast)
+        }
+    }
+}
